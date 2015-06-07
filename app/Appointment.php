f6e5eccc9eab662b25bbbaf75d71e7e1bc0fda2d
Robert Hartman <hart_dev@yahoo.com>
2015-06-07 14:44:18 -0400

    Change Appointments table to appointments
    Having Appointments as the name of the table worked on
    Windows/XAMPP/MySQL.  However, Linux MySQL does not like the capital A
    in Appointment. Changed Appointments to appointments

--- app/Appointment.php
+++ app/Appointment.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Eloquent\Model;
 class Appointment extends Model
 {
 
-	protected $table = 'Appointments';
+	protected $table = 'appointments';
 
 	protected $fillable = ['start_time', 'end_time','first_name', 'last_name','comment'];
 	//protected $hidden = ['id','created_at','updated_at'];
